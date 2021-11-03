422b0834ea99e2aae383b4a8f67dba42f18c2c4c
Jeremy Miller <jeremy.miller@wirecutter.com>
2021-11-03 13:22:31 -0700

    prettier formatting

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 module.exports.responseHooks = [
-  context => {
-    var body = context.response.getBody();
-    let data = JSON.parse(Buffer.from(body).toString());
-    let maybeUnstringifiedData = data;
+    (context) => {
+        var body = context.response.getBody();
+        let data = JSON.parse(Buffer.from(body).toString());
+        let maybeUnstringifiedData = data;
 
-    const unstringifiedData = data => {
-        let clonedData = { ...data };
-        let entries = Object.entries(clonedData);
+        const unstringifiedData = (data) => {
+            let clonedData = { ...data };
+            let entries = Object.entries(clonedData);
 
-        entries.forEach(([key, value]) => {
-          if (typeof value === 'object') {
-            clonedData[key] = unstringifiedData(value)
-          }
-          if (typeof value === 'string') {
-            try {
-              clonedData[key] = JSON.parse(value);
-            } catch (e) {
-              clonedData[key] = value;
-            }
-          }
-        })
-      return clonedData;
-    }
+            entries.forEach(([key, value]) => {
+                if (typeof value === 'object') {
+                    clonedData[key] = unstringifiedData(value);
+                }
+                if (typeof value === 'string') {
+                    try {
+                        clonedData[key] = JSON.parse(value);
+                    } catch (e) {
+                        clonedData[key] = value;
+                    }
+                }
+            });
+            return clonedData;
+        };
 
-    if (typeof data === 'object') {
-      maybeUnstringifiedData = unstringifiedData(data);
-    }
+        if (typeof data === 'object') {
+            maybeUnstringifiedData = unstringifiedData(data);
+        }
 
-    const formattedData = Buffer.from(JSON.stringify(maybeUnstringifiedData));
-    context.response.setBody(formattedData);
-  }
-]
+        const formattedData = Buffer.from(
+            JSON.stringify(maybeUnstringifiedData)
+        );
+        context.response.setBody(formattedData);
+    },
+];
